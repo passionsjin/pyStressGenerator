c3cf678f0f5e7e297a797e6d7433c5f75f9a4271
passionsjin <passionsjin@hanwha.com>
2022-09-07 14:55:59 +0900

    ENV 변경

--- app_config.py
+++ app_config.py
@@ -3,7 +3,7 @@ import os
 
 class AppConfig:
     CPU_CORES = os.getenv('CPU_CORES', None)
-    MEMORY = os.getenv('MEMORY', None)
+    MEMORY_MB = os.getenv('MEMORY_MB', None)
     CPU_PERCENT = os.getenv('CPU_PERCENT', 90)
 
 
